3544b180784a259d47d355e13f1385601c135291
Savaş Tanrıverdi <savas.tanriverdi@outlook.com>
2025-02-17 21:23:49 +0300

    Trasform.Translate()

--- Assets/Driver.cs
+++ Assets/Driver.cs
@@ -4,11 +4,13 @@ public class Driver : MonoBehaviour
 {
     void Start()
     {
+
     }
 
     void Update()
     {
         transform.Rotate(0,0,.1f);
+        transform.Translate(0, .01f, 0);
         
     }
 }
