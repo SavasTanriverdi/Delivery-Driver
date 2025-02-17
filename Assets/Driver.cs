8f6f0d744f0412625827325e92fab7f0fd930bb5
Savaş Tanrıverdi <savas.tanriverdi@outlook.com>
2025-02-18 00:42:13 +0300

    Using Time.deltaTime()

--- Assets/Driver.cs
+++ Assets/Driver.cs
@@ -11,8 +11,8 @@ public class Driver : MonoBehaviour
 
     void Update()
     {
-        float steerAmount = Input.GetAxis("Horizontal") * steerSpeed; // -1 to 1 for left and right
-        float moveAmount = Input.GetAxis("Vertical") * moveSpeed; // -1 to 1 for forward and back
+        float steerAmount = Input.GetAxis("Horizontal") * steerSpeed * Time.deltaTime; // -1 to 1 for left and right
+        float moveAmount = Input.GetAxis("Vertical") * moveSpeed * Time.deltaTime; // -1 to 1 for forward and back
         transform.Rotate(0,0,-steerAmount);
         transform.Translate(0, moveAmount, 0);
     }
