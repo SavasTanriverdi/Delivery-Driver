caa338781c1d49fd00e324784aa9b636dbc87b64
Savaş Tanrıverdi <savas.tanriverdi@outlook.com>
2025-02-18 18:24:28 +0300

    How To Use Bools

--- Assets/Delivery.cs
+++ Assets/Delivery.cs
@@ -2,6 +2,12 @@ using UnityEngine;
 
 public class Delivery : MonoBehaviour
 {
+    bool hasPackage = false;
+
+    void Start()
+    {
+        Debug.Log(hasPackage);
+    }
     void OnCollisionEnter2D(Collision2D other)
     {
         Debug.Log("Ouch! I hit something!");
@@ -12,11 +18,13 @@ public class Delivery : MonoBehaviour
         if (other.tag == "Package")
         {
             Debug.Log("Package picked up!");
+            hasPackage = true;
         }
 
-        if (other.tag == "Customer")
+        if (other.tag == "Customer" && hasPackage)
         {
-            Debug.Log("Package delivered!");
+            Debug.Log("Customer is happy!");
+            hasPackage = false;
         }
     }
 }
